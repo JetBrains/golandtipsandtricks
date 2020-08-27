e39f50aa1ba7787dc0f3c650385bab491f1cede2
Florin Patan <florinpatan@gmail.com>
2020-08-27 18:21:40 +0300

    Add tip about quick definition

--- navigation/tip003/main.go
+++ navigation/tip003/main.go
@@ -6,9 +6,14 @@ func main() {
 	// Step 1. Invoke Type Hierarchy on the Reader type.
 	// Shortcut: Ctrl + H on Windows/Linux
 	//           ^ + H on macOS
+
 	// Step 2. Invoke Call Hierarchy on the Read method.
 	// Shortcut: Ctrl + Alt + H on Windows/Linux
 	//           ⌥ + ^ + H on macOS
+
+	// Step 3. Use Quick Definition on any types to preview them
+	// Shortcut: Ctrl + Shift + Space on Windows/Linux
+	//           ⌥ + Space on macOS
 
 	_, _ = io.Reader(nil).Read(nil)
 }
